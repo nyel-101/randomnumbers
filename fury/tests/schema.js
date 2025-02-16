29e9d53ad9d916344eb3c6e912148f3e88613450
nyel-101 <dandevjr@gmail.com>
2025-02-16 08:01:28 +0000

    style(ui): sanitize middleware routes

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_016fa430aa4b492287f72d5e6868698b = () => {
-  return '2026-06-02T00:16:14Z';
+const node_1b1f2718888e486a92517ba1f8167ed3 = () => {
+  return '2025-02-16T08:01:28Z';
 };
