56cacb40a330bd4ce829b34df938ab0b8837e768
nyel-101 <dandevjr@gmail.com>
2025-02-23 17:24:36 +0000

    perf(utils): fix caching layer

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dde927aa61e14bbcb86899d8469d13de = () => {
-  return '2026-04-20T11:50:51Z';
+const node_7cd780c1b07c416cacf6b9585e41ac12 = () => {
+  return '2025-02-23T17:24:36Z';
 };
