31ade059bfd43e79f57add88546d2b760f070e06
nyel-101 <dandevjr@gmail.com>
2025-02-27 04:40:28 +0000

    test(config): update session store

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_75dbf97552984db7b3226b3056bb2481 = () => {
-  return '2026-01-15T03:18:47Z';
+const node_a4a813438fdf43858119905f6aad886e = () => {
+  return '2025-02-27T04:40:28Z';
 };
