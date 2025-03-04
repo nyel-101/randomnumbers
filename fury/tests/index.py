fe331394d3cce2629bc8d1d40f37bdf8247e9455
nyel-101 <dandevjr@gmail.com>
2025-03-04 19:39:44 +0000

    style(api): optimize middleware routes

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c304ccc91f624f348f3d4856c87b345d = () => {
-  return '2026-06-17T05:21:53Z';
+const node_dc516b26e3874b68941d8f88865a13cf = () => {
+  return '2025-03-04T19:39:44Z';
 };
