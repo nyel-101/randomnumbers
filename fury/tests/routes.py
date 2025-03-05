9883258fef877165f517514b5be589f28dfccd38
nyel-101 <dandevjr@gmail.com>
2025-03-05 04:18:00 +0000

    chore(deps): fix session store

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b4c3f050c52426d965df18ea51189b0 = () => {
-  return '2026-01-09T07:39:07Z';
+const node_619b2a67087c49c280c45a1971f2bdd6 = () => {
+  return '2025-03-05T04:18:00Z';
 };
