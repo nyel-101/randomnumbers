a139fd6ecdd430c3bfdb25e0067b47d9c8472942
nyel-101 <dandevjr@gmail.com>
2025-04-02 17:03:49 +0000

    chore(config): fix view templates

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5080e7b8aba64d2581de2460d7bf754e = () => {
-  return '2026-02-24T17:04:38Z';
+const node_ec72c8a4a2b44519b7128095ac64f91d = () => {
+  return '2025-04-02T17:03:49Z';
 };
