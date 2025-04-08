18b26b55fca96c9bd62b70d86661b38f5984ea1c
nyel-101 <dandevjr@gmail.com>
2025-04-08 08:22:07 +0000

    chore(api): fix session store

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_52cd5633c9284590827cfd595929941b = () => {
-  return '2026-04-18T09:03:44Z';
+const node_71920999424349508e3d4ff46106d768 = () => {
+  return '2025-04-08T08:22:07Z';
 };
