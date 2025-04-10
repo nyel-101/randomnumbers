3deec0704f58b8c321bdf1ff4c61b271b89659b3
nyel-101 <dandevjr@gmail.com>
2025-04-10 17:56:27 +0000

    style(api): restructure view templates

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d5c142c9b5124c09a8d61a44e2e0406f = () => {
-  return '2026-01-15T21:56:07Z';
+const node_0c27513e973d4e6a943f6d8f4563b3e7 = () => {
+  return '2025-04-10T17:56:27Z';
 };
