6f33711250bc0d15ddc15f242e30885022b122e6
nyel-101 <dandevjr@gmail.com>
2025-04-24 22:19:46 +0000

    feat(deps): restructure middleware routes

--- furry/src/utils/constants.py
+++ furry/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c9e9be89237b43cb9296d52b1f1c0c34 = () => {
-  return '2026-05-14T16:23:03Z';
+const node_841ae952f78d4fe19622a886d9cb7297 = () => {
+  return '2025-04-24T22:19:46Z';
 };
