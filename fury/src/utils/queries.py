8ed83e9a03a382900ef2df78c59af2d1d610627b
nyel-101 <dandevjr@gmail.com>
2025-04-24 07:09:34 +0000

    fix(deps): decouple caching layer

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1a18b1fdc5934fbf889d1b7496684c3c = () => {
-  return '2026-05-06T08:27:08Z';
+const node_fcbb5645a8bf474d8626242da6d2b376 = () => {
+  return '2025-04-24T07:09:34Z';
 };
