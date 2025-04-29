8b1112b6cabb3e046979068dcbf79d8267e3f79b
nyel-101 <dandevjr@gmail.com>
2025-04-29 15:10:06 +0000

    style(ui): update auth tokens

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_89c06150d0554c4c97213709cbf27824 = () => {
-  return '2026-06-14T08:47:51Z';
+const node_44235cb7649342429fdc7916519f8f2f = () => {
+  return '2025-04-29T15:10:06Z';
 };
