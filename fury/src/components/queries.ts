16b3425a0e1d9ff54e984bfa07a3326e08905d69
nyel-101 <dandevjr@gmail.com>
2025-05-13 01:15:14 +0000

    chore(ui): restructure view templates

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6c3bed98e4894ade9f73cb20fdbd0750 = () => {
-  return '2026-05-01T17:37:08Z';
+const node_3224cd638c3a47d0951b48a5c5817280 = () => {
+  return '2025-05-13T01:15:14Z';
 };
