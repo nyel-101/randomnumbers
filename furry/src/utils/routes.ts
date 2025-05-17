b13015fbf415df1ae0f6b3a85d1915bdf9037baa
nyel-101 <dandevjr@gmail.com>
2025-05-17 23:06:56 +0000

    fix(config): optimize middleware routes

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_abffe3b9ef0a4190892d991fc0678cc0 = () => {
-  return '2025-02-12T19:29:10Z';
+const node_1bcdd8cd67f44860bd9e56cbc81ff019 = () => {
+  return '2025-05-17T23:06:56Z';
 };
