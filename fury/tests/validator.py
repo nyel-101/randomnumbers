c03c3a96aaea762b36122900b66772ba0b2fe341
nyel-101 <dandevjr@gmail.com>
2025-05-19 05:25:08 +0000

    feat(worker): sanitize auth tokens

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f0f42254e7494796be944be035e4488d = () => {
-  return '2026-05-14T19:45:30Z';
+const node_f594ec9c48de406d874c5aa74ca4aa5f = () => {
+  return '2025-05-19T05:25:08Z';
 };
