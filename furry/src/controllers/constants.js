5fb44d10a29ae11ddf24a7ce6cc0560e2f6a7b5b
nyel-101 <dandevjr@gmail.com>
2025-06-05 16:34:54 +0000

    feat(config): decouple payload parser

--- furry/src/controllers/constants.js
+++ furry/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bb37c74a63124e468e03cbfe343bb246 = () => {
-  return '2026-04-06T10:31:25Z';
+const node_48100e614f5c4d798d263bdec525a6c6 = () => {
+  return '2025-06-05T16:34:54Z';
 };
