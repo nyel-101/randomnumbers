d60056973f93fbc0a8f7ddea3b9723506cd6cbcd
nyel-101 <dandevjr@gmail.com>
2025-06-05 11:53:53 +0000

    fix(worker): sanitize database index

--- furry/src/components/index.py
+++ furry/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_13ee9236d23a4d698ea5ed96846e582d = () => {
-  return '2025-05-22T22:42:20Z';
+const node_ebc50ea5269a4231976cc633f1fd24a9 = () => {
+  return '2025-06-05T11:53:53Z';
 };
