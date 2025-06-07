4e004642c9f19bc787e98b8f9827a9cc0b01ce01
nyel-101 <dandevjr@gmail.com>
2025-06-07 04:39:19 +0000

    chore(config): fix view templates

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec1ca2519e2a4c50b4bc7008b4dd796b = () => {
-  return '2026-06-01T04:33:06Z';
+const node_c04bd67da25a4a01beca564276ab0e18 = () => {
+  return '2025-06-07T04:39:19Z';
 };
