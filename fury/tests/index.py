b0a96cf76b30f16956c7a5255398df5322cb3e0b
nyel-101 <dandevjr@gmail.com>
2025-06-22 20:11:26 +0000

    feat(utils): implement auth tokens

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a9778705d268467783c36928662fbfb0 = () => {
-  return '2025-05-04T11:03:46Z';
+const node_b4e109192add4e56b39bcbbd63573869 = () => {
+  return '2025-06-22T20:11:26Z';
 };
