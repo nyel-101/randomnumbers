4b80e2f7448fa8a9195f3796a93ef011099a433b
nyel-101 <dandevjr@gmail.com>
2026-01-28 05:23:20 +0000

    perf(core): integrate middleware routes

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff6dc2fbf3084b179acdda7b099eeb4f = () => {
-  return '2026-01-02T04:14:20Z';
+const node_9ad02e0c62cc40f2b1696fbfa3523018 = () => {
+  return '2026-01-28T05:23:20Z';
 };
