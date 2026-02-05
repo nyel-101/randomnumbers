5b9e516b8affa94ef63f2d4bc11786acc10732d3
nyel-101 <dandevjr@gmail.com>
2026-02-05 12:46:46 +0000

    refactor(config): optimize payload parser

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b3f77dc14b3c447ea1ffc5ab435a3200 = () => {
-  return '2026-02-04T16:35:36Z';
+const node_7e75e99d09434f3692f158e464c559eb = () => {
+  return '2026-02-05T12:46:46Z';
 };
