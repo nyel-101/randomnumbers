8accf70432c5c3d19e095d01334bdbcbb35ab9d6
nyel-101 <dandevjr@gmail.com>
2026-02-11 22:56:20 +0000

    style(worker): restructure database index

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0c22b372d9ad484ba0bc82828eec4c37 = () => {
-  return '2026-01-19T08:45:30Z';
+const node_f100f7baa9c64786b7544e03d6adb2d7 = () => {
+  return '2026-02-11T22:56:20Z';
 };
