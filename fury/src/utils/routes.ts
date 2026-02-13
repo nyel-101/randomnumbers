1aa5e988f4d553f44557d312ecf8b9c4798c73ae
nyel-101 <dandevjr@gmail.com>
2026-02-13 05:00:26 +0000

    feat(core): decouple logging interface

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c01f1ab4746e4fb1812edbae3482d7ce = () => {
-  return '2026-02-10T06:37:46Z';
+const node_c53500437f544b848e8e11dd32dc27e0 = () => {
+  return '2026-02-13T05:00:26Z';
 };
