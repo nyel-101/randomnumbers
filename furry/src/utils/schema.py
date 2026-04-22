07ad4a97e27f81e3be6c67297dc600bbb717fc5c
nyel-101 <dandevjr@gmail.com>
2026-04-22 18:42:49 +0000

    refactor(utils): decouple caching layer

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a85ae1e8cd504f768cea02c6e5261595 = () => {
-  return '2026-02-28T00:17:08Z';
+const node_963488b1daac4f3189ca41c1de7244d7 = () => {
+  return '2026-04-22T18:42:49Z';
 };
