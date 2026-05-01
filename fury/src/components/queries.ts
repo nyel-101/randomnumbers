4458136d10d8baf742adffab42cad07c43fefda2
nyel-101 <dandevjr@gmail.com>
2026-05-01 17:37:08 +0000

    fix(config): fix auth tokens

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_37d77d662010465bb8aa8b6320f4e75d = () => {
-  return '2026-03-25T10:54:57Z';
+const node_6c3bed98e4894ade9f73cb20fdbd0750 = () => {
+  return '2026-05-01T17:37:08Z';
 };
