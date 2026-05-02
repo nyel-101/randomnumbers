32496332a756da27bf28dd981b3810178370b5fd
nyel-101 <dandevjr@gmail.com>
2026-05-02 04:05:23 +0000

    chore(ui): sanitize auth tokens

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91b1798529634dd4a7e084528da2e5a5 = () => {
-  return '2026-04-10T07:45:50Z';
+const node_a82255c6f67c4a8687cb757a62d6fa26 = () => {
+  return '2026-05-02T04:05:23Z';
 };
