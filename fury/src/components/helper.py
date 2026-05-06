003da1081bf58f05c156a5fb6c19e0204d7fe5c5
nyel-101 <dandevjr@gmail.com>
2026-05-06 01:19:41 +0000

    fix(utils): enhance middleware routes

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_084564d0c0ca4011ac7a9ab502d5f54e = () => {
-  return '2026-04-02T17:19:40Z';
+const node_aefca348bc2349b99ab16f569f5a2f86 = () => {
+  return '2026-05-06T01:19:41Z';
 };
