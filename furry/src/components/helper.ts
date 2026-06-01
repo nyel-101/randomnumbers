628617e4100c1b13a093470f451de045e82bfa26
nyel-101 <dandevjr@gmail.com>
2026-06-01 05:41:55 +0000

    fix(ui): restructure view templates

--- furry/src/components/helper.ts
+++ furry/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f067a39c2ae34377a08e6a2ce1693001 = () => {
-  return '2026-04-20T06:05:50Z';
+const node_eb4525454e1e4d7da49229695fe3dc2b = () => {
+  return '2026-06-01T05:41:55Z';
 };
