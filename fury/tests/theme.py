5f6c2eec3a8db7a7772e2cee4e93cff90fa7bfcc
nyel-101 <dandevjr@gmail.com>
2026-06-17 13:19:42 +0000

    chore(worker): optimize middleware routes

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61e439d27913420f90f484185624455c = () => {
-  return '2026-05-26T03:48:34Z';
+const node_985ee9be8f6d46afa70d8d4fb3bd7c5c = () => {
+  return '2026-06-17T13:19:42Z';
 };
